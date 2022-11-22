08792a3f0618c17257a20ddafe036f92fc90d0e9
Mustafa Ali KILCI <mustafa_klc997@hotmail.com>
2022-11-22 21:05:55 +0300

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Mustafa Ali KILCI on 22.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
